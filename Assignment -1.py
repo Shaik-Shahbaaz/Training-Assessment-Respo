c119cc972eb6e93c60540b8dcefbf06d5b1c1893
Shaik Shahbaaz <shaikshahbaaz6362@gmail.com>
2025-11-06 21:11:54 +0530

    Add files via upload

---
+++ Assignment -1.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+print(a)
+
+
+
+
+# In[2]:
+
+
+a,b,c=5,10,15
+print(a,b,c)
+
+
+# In[3]:
+
+
+name = "Shaik Shahbaaz"
+print(name)
+
+
+# In[4]:
+
+
+fir_na="shaik"
+las_na="shahbaaz"
+print(fir_na,las_na)
+
+
+# In[7]:
+
+
+age =21
+print("i am",21,"years old")
+
+
+# In[11]:
+
+
+name = "ravi"
+city = "mumbai"
+print(f"My name is {name} and I live in {city}")
+
+
+# In[16]:
+
+
+a,b,c=10,20,30
+print(a,"|",b,"|",c)
+
+
+# In[17]:
+
+
+print(" python and Rocks!")
+
+
+# In[19]:
+
+
+a=5
+b=8
+print(f"{a+b}")
+
+
+# In[20]:
+
+
+user= input("enter your name")
+print("Hello",user)
+
+
+# In[22]:
+
+
+city= "hyd"
+print(city)
+
+
+# In[26]:
+
+
+a=10
+b=20
+a,b=b,a
+print(a,b)
+
+
+# In[27]:
+
+
+a,b,c=5,10,"python"
+print(a,b,c)
+
+
+# In[28]:
+
+
+color1="Red"
+color2="Red"
+color3="Red"
+
+
+# In[30]:
+
+
+num = 3.14 
+print( num,type(num))
+
+
+# In[33]:
+
+
+name= "sk"
+age=21
+print(f"my name is {name} and i am {age} years old")
+
+
+# In[34]:
+
+
+555
+
+
+# In[35]:
+
+
+is_python_fun= True
+print(is_python_fun)
+
+
+# In[36]:
+
+
+15
+
+
+# In[39]:
+
+
+x=5
+y=7
+print(x+y)
+
+
+# In[41]:
+
+
+a=10
+b=30
+a,b=b,a
+print(a,b)
+
+
+# In[42]:
+
+
+greeting="hello"
+print(greeting)
+
+
+# In[43]:
+
+
+result=5*8
+print(result)
+
+
+# In[44]:
+
+
+a,b=7,8
+print(a,b)
+
+
+# In[45]:
+
+
+f_name="shaik"
+l_name="shahbaaz"
+print(f_name,l_name)
+
+
+# In[46]:
+
+
+num=8
+num=num+5
+print(num)
+
